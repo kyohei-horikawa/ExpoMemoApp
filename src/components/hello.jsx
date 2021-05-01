44ef618c50270cd9d73265a2ca3a03baf756fd74
Kyohei Horikawa <kyohei3430@gmail.com>
2021-05-01 13:38:07 +0900

    Add prettierc

---
+++ src/components/hello.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> hello </Text>
+      </View>
+    );
+  }
+}
+
+export default hello;
