1674b017ccf07eebae132a22d03b2030a2e8512c
Kyohei Horikawa <kyohei3430@gmail.com>
2021-05-01 16:27:29 +0900

    Add EKeyBoardSafeView

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  KeyboardAvoidingView,
-} from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeyBoardSafeView from '../components/KeyBoardSafeView';
 
 export default function MemoEditScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyBoardSafeView style={styles.container} behavior="height">
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyBoardSafeView>
   );
 }
 
